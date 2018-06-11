fef0bd4441a9b5722b6a9ee8ceb6438f4721baf2
Alex Chaffee <alexch@gmail.com>
2018-06-11 10:06:33 -0700

    binaryToDecimal to function

--- exercises/binary/binary.rot13.js
+++ exercises/binary/binary.rot13.js
@@ -1,16 +1,12 @@
 'hfr fgevpg';
 
-zbqhyr.rkcbegf = Ovanel;
-
-shapgvba Ovanel(ovanel) {
+zbqhyr.rkcbegf = shapgvba(ovanel) {
   vs (ovanel.zngpu(/^[01]*$/)) {
-    guvf.ovanel = cnefrVag(ovanel, 2);
+    ovanel = cnefrVag(ovanel, 2);
   } ryfr {
-    guvf.ovanel = 0;
+    ovanel = 0;
   }
-}
-
-Ovanel.cebgbglcr.gbQrpvzny = shapgvba () {
-  ine bhg = Ahzore(guvf.ovanel.gbFgevat(10));
+  ine bhg = Ahzore(ovanel.gbFgevat(10));
   erghea vfAnA(bhg) ? 0 : bhg;
 };
+
--- exercises/binary/binary.spec.js
+++ exercises/binary/binary.spec.js
@@ -1,47 +1,47 @@
-var Binary = require('./binary');
+var binaryToDecimal = require('./binary');
 
 describe('binary', function () {
   it('0 is decimal 0', function () {
-    expect(new Binary('0').toDecimal()).toEqual(0);
+    expect(binaryToDecimal('0')).toEqual(0);
   });
 
   it('1 is decimal 1', function () {
-    expect(new Binary('1').toDecimal()).toEqual(1);
+    expect(binaryToDecimal('1')).toEqual(1);
   });
 
   it('10 is decimal 2', function () {
-    expect(new Binary('10').toDecimal()).toEqual(2);
+    expect(binaryToDecimal('10')).toEqual(2);
   });
 
   it('11 is decimal 3', function () {
-    expect(new Binary('11').toDecimal()).toEqual(3);
+    expect(binaryToDecimal('11')).toEqual(3);
   });
 
   it('100 is decimal 4', function () {
-    expect(new Binary('100').toDecimal()).toEqual(4);
+    expect(binaryToDecimal('100')).toEqual(4);
   });
 
   it('1001 is decimal 9', function () {
-    expect(new Binary('1001').toDecimal()).toEqual(9);
+    expect(binaryToDecimal('1001')).toEqual(9);
   });
 
   it('11010 is decimal 26', function () {
-    expect(new Binary('11010').toDecimal()).toEqual(26);
+    expect(binaryToDecimal('11010')).toEqual(26);
   });
 
   it('10001101000 is decimal 1128', function () {
-    expect(new Binary('10001101000').toDecimal()).toEqual(1128);
+    expect(binaryToDecimal('10001101000')).toEqual(1128);
   });
 
   it('00011111 is decimal 31', function () {
-    expect(new Binary('00011111').toDecimal()).toEqual(31);
+    expect(binaryToDecimal('00011111')).toEqual(31);
   });
 
   it('invalid inputs are decimal 0', function () {
-    expect(new Binary('carrot').toDecimal()).toEqual(0);
-    expect(new Binary('012').toDecimal()).toEqual(0);
-    expect(new Binary('10nope').toDecimal()).toEqual(0);
-    expect(new Binary('nope10').toDecimal()).toEqual(0);
-    expect(new Binary('10nope10').toDecimal()).toEqual(0);
+    expect(binaryToDecimal('carrot')).toEqual(0);
+    expect(binaryToDecimal('012')).toEqual(0);
+    expect(binaryToDecimal('10nope')).toEqual(0);
+    expect(binaryToDecimal('nope10')).toEqual(0);
+    expect(binaryToDecimal('10nope10')).toEqual(0);
   });
 });
